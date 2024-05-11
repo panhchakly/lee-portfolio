27611f27345f42c891dc13a3f93fd080ad3c45de
Chheng Panhchakly <chhengpanhchakly@gmail.com>
2024-05-11 10:45:17 +0700

    navbar and info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Navbar from "./Components/Navbar/Navbar";
+import "./App.css";
+import Intro from "./Components/Intro/Intro";
+
 function App() {
   return (
     <div className="App">
-      CHHENG PANHCHAKLY
+      <Navbar />
+      <Intro />
     </div>
   );
 }
